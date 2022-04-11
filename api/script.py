d650951a712bdccf8a30d78889b5159851c7ef7d
Ben Weinel <bweinel@udel.edu>
2022-04-10 20:51:37 -0400

    added map component

--- api/script.py
+++ api/script.py
@@ -1,6 +1,6 @@
 from api import db
 db.create_all()
 from api import Fighter
-fighter = Fighter(hydration = 75, oxygen = 99, heartrate = 65, floor = 4, latitude = 39.68014, longitude = -75.75125, map = "map1", name = "Billy Bob")
+fighter = Fighter(hydration = "75", oxygen = "99", heartrate = "65", floor = "4", latitude = "39.68014", longitude = "-75.75125", map = "map1", name = "Ben Weinel", age = "21", height = "72", weight = "200", sex = "male")
 db.session.add(fighter)
 db.session.commit()
