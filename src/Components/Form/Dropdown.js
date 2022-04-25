54b85a6d07956926d52c3a290f38a1cb3623c99f
Ben Weinel <bweinel@udel.edu>
2022-04-25 15:06:15 -0400

    Added Dropdown menu for sex

---
+++ src/Components/Form/Dropdown.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Dropdown.css'
+class DropdownComp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: 'male'};
+      this.onFormSexDrop = this.onFormSexDrop.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    onFormSexDrop = (event) => {
+        this.props.onFormSexDrop(event)
+    }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+      this.onFormSexDrop(event.target.value);
+    }
+  
+    render() {
+      return (
+            <select id="dropdown" value={this.state.value} onChange={this.handleChange}>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+      );
+    }
+  }
+  export default DropdownComp
